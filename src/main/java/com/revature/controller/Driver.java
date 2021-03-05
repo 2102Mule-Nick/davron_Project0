6994f24103e39b995fb1b6705581d7e02206c654
Davron Tairov <davrontairov@gmail.com>
2021-03-05 00:46:18 -0800

    First Commit

---
+++ src/main/java/com/revature/controller/Driver.java
@@ -0,0 +1,16 @@
+package com.revature.controller;
+
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+public class Driver {
+	private static Logger log = Logger.getLogger(Driver.class);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+	}
+
+}
